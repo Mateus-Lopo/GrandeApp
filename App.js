66b99298102c3a0f8388eb2018efe20d6b12243f
Mateus Lopo <mateuslop26@outlook.com>
2025-09-04 23:35:45 -0300

    Update App.js

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Meu App</Text>
       <BotaoVermelho/>
+      <Text>Isso é um botão vermelho</Text>
     </View>
   );
 }
